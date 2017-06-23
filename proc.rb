03957b6719bf5fa660bcd1b36140b9ac11367258
durenshi <durenshi1992@163.com>
2017-06-23 17:11:01 +0800

    proc lamdba学习
    proc lamdba学习

---
+++ proc.rb
@@ -0,0 +1,7 @@
+my_proc = Proc.new {|n| puts "proc call #{n}"}
+my_proc.call(1)
+my_proc.call()
+
+my_lambda = lambda { |n| puts "lambda call #{n}"}
+my_lambda.call(2) 
+my_lambda.call() # 报错。Lambda对参数的检查很严格，而Proc则比较宽松
